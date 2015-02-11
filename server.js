4a720ce946d117415342f71bc259ecc7d8663835
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-02-11 23:23:34 +0400

    basic routing in place and implemented a route to add new photos

--- server.js
+++ server.js
@@ -4,11 +4,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;
 
 // Mongo DB Connection
 var mongoose   = require('mongoose');
@@ -17,23 +16,51 @@ mongoose.connection.on("open", function(){
 	console.log("Connected to Photare database");
 });
 
+// Models
+var Photo = require('./app/models/photo.js');
+
+
 /**
  * Routes
  */
 var router = express.Router();
 
+// Middleware for routes
+router.use(function(req, res, next){
+	console.log("New API request");
+	next();
+});
+
+// First route
 router.get('/', function(req, res){
 	res.json({message: 'Welcome to Photare'});
 });
 
+router.route('/photos')
+	// Create a photo
+	.post(function(req, res){
+		var photo = new Photo();
+		photo.title = req.body.title;
+		photo.image = "";
+		photo.url = "http://rahul.local:8080/api/photos/1";
+
+		photo.save(function(err){
+			if(err){
+				res.send(err);
+			}
+			res.json({message: "Photo created!"});
+		});
+		
+	});
+
 
 // Define api endpoint
-
 app.use('/api', router);
+
 
 /**
  * Start the server
- * */
+ */
 
 app.listen(port);
 console.log("Listening...");
