510951b64da7107fb67a3398e00ef0362e1cd09b
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-02-17 00:16:10 +0400

    User model created for authentication

---
+++ app/models/user.js
@@ -0,0 +1,11 @@
+var mongoose = requre('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+	local: {
+		email: 		String,
+		password: 	String,
+	}
+});
+
+module.exports = mongoose.model('User', UserSchema)
