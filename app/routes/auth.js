689f01d1495874ff107fedeaaaf28f96a2bf53c6
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-02-19 23:52:58 +0400

    basic auth test - test failure done

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -2,6 +2,8 @@
 var Photo = require('../models/photo.js');
 var express = require('express');
 var authRouter = express.Router();
+var passport = require('passport');
+require('../lib/passport.js');
 
 // Middleware for routes
 authRouter.use(function(req, res, next){
@@ -13,5 +15,10 @@ authRouter.use(function(req, res, next){
 authRouter.get('/', function(req, res){
 	res.json({message: 'Please login or register to continue'});
 });
+
+authRouter.post('/login',
+	passport.authenticate('local', { successRedirect: '/success',
+		failureRedirect: '/failure'})
+);
 
 module.exports = authRouter;
