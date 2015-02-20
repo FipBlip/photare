ea95432ac513066e7d7f40c6c1c094e1d295f011
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-02-21 01:30:44 +0400

    added sample registration route + minor refactor

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,8 +1,10 @@
 // Models
 var Photo = require('../models/photo.js');
+var User = require('../models/user.js');
 var express = require('express');
 var authRouter = express.Router();
 var passport = require('passport');
+var app = express();
 require('../lib/passport.js');
 
 // Middleware for routes
@@ -17,8 +19,25 @@ authRouter.get('/', function(req, res){
 });
 
 authRouter.post('/login',
-	passport.authenticate('local', { successRedirect: '/success',
-		failureRedirect: '/failure'})
+	passport.authenticate('local', { 
+		successRedirect: '/success',
+		failureRedirect: '/failure'
+	})
+);
+
+authRouter.post('/register',
+	function(req, res){
+		var user = new User();
+		user.local.username = "theunexpected1";
+		user.local.email = "rahul.vagadiya@gmail.com";
+		user.local.password = "123.com";
+		user.save(function(err){
+			if(err){
+				res.send(err);
+			}
+			res.json({message: "user created!"});
+		});
+	}
 );
 
 module.exports = authRouter;
