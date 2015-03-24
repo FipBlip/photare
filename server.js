9e803e107ca57f2acd7c3a52fec62ff678501474
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-03-25 03:30:14 +0400

    moved config + public view route added + sample react view added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var multer = require('multer'); 
 var oknow = require('oknow');
 var mongoose = require('mongoose');
-var config = require('./config/index.js');
+var config = require('./app/config/index.js');
 var models = require('./app/models/index.js');
 var flash = require('connect-flash');
 
@@ -18,15 +18,14 @@ var System = {
 	config: config,
 	oknow: oknow
 };
+var apiRouter = require('./app/routes/api.js')(System);
+var authRouter = require('./app/routes/auth.js')(System);
 
 // Middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(multer({ dest: config.uploadsDir}));
 app.use(flash());
-
-var apiRouter = require('./app/routes/api.js')(System);
-var authRouter = require('./app/routes/auth.js')(System);
 
 // Mongo DB Connection
 mongoose.connect('mongodb://localhost/photare'); // connect to our database
@@ -38,10 +37,11 @@ mongoose.connection.on("error", function(){
 });
 
 
-// Define api endpoint
+// Routes - Server - Define auth & api endpoint
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
 
+// Routes - Server - auth
 // ToDo: Refactor & Move into auth
 app.get('/successRegister', function(req, res){
 	res.json({message: "success after Register"});
@@ -55,6 +55,13 @@ app.get('/successLogin', function(req, res){
 app.get('/failureLogin', function(req, res){
     console.log(req.flash('error'));
 	res.json({message: "failure after login"});
+});
+
+// Routes - Client - Set /public as our static content dir
+app.use('/', express.static(__dirname + '/public/'));
+app.get('*', function(req, res){
+	res.setHeader('Content-Type', 'text/html');
+	res.sendfile('./app/public/views/index.html');
 });
 
 
