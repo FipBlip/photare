47177bbee9ddee5fde0c18d1461efe3d069c0dc2
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2015-03-26 03:41:09 +0400

    grunt tasks added for browserify, less, and concurrent watching. Also, dev/dist builds added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,116 @@
 'use strict';
 
+// grunt --dev
+// grunt --dist
+// grunt build --dev
+// grunt build --dist
+
 module.exports = function(grunt){
+	
+	var mode = grunt.option('dist') ? 'dist' : 'dev';
 
 	grunt.initConfig({
 		jshint: {
+			options: {
+				ignores: ['app/public/js/bundle.js', 'app/public/js/MyRaisedButton.js', 'app/public/js/index.js']
+			},
 			all: {
 				src: ['app/**/*.js', 'server.js']
 			}
 		},
 
+		browserify: {
+			dev: {
+				options: {
+					watch: true, // use watchify for incremental builds!
+					keepAlive: true,
+					debug: true,
+					transform: ['reactify']
+				},
+				src: ['app/public/js/index.js'],
+				dest: 'app/public/js/bundle.js'
+			},
+			dist: {
+				options: {
+					debug: false,
+					transform: ['reactify']
+				},
+				src: ['<%= browserify.dev.src%>'],
+				dest: '<%= browserify.dev.dest%>'
+			}
+		},
+
+		/*
+		sass: {
+			dev: {
+				options: {
+					style: 'expanded'
+				},
+				src: ['app/public/css/main.scss'],
+				dest: 'app/public/css/main.css'
+			}
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				src: '<%=sass.dev.src%>',
+				dest: '<%=sass.dev.dest%>',
+			}
+		},
+		*/
+
+		less: {
+			dev: {
+				src: ['app/public/css/main.less'],
+				dest: 'app/public/css/main.css'
+			},
+			dist: {
+				src: '<%=sass.dev.src%>',
+				dest: '<%=sass.dev.dest%>',
+			}
+		},
+
 		watch: {
-			all: {
-				files: ['app/**/*.js', 'server.js', 'Gruntfile.js'],
+			css: {
+				files: ['app/**/*.less'],
+				tasks: ['less:' + mode]
+			},
+			/*
+			rest: {
+				files: ['Gruntfile.js'],
 				tasks: ['default']
+			}
+			*/
+		},
+
+		concurrent: {
+			dev: {
+				options: {
+					logConcurrentOutput: true
+				},
+				tasks: ['browserify:' + mode, 'watch:css']
+			},
+			dist: {
+				options: {
+					logConcurrentOutput: true
+				},
+				tasks: ['browserify:' + mode, 'watch:css']
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-concurrent');
 
-	grunt.registerTask('default', ['jshint']);
+	// linting and others (currently none as jshint doesn't support jsx yet (in my implementation))
+	grunt.registerTask('preTasks', []);
+
+	// building and concurrent tasks
+	grunt.registerTask('build', ['preTasks', 'concurrent:' + mode]);
+
+	grunt.registerTask('default', ['build']);
+
 };
